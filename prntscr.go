6d6de2c5a9e44cf455423589bacc99ef43088703
Mike M <mike@makarov.pw>
2020-04-19 07:56:14 +0300

    cycle

--- prntscr.go
+++ prntscr.go
@@ -9,22 +9,25 @@ import (
 )
 
 func main() {
-	path := randomizer.String(6)
-	//fmt.Println(path)
-	//s := "https://prnt.sc/"
-	//download.File(s+path, path+".html")
-	/*if err := download.File("https://prnt.sc/"+path, path+".html"); err != nil {
-		panic(err)
-	}*/
-	imgURL, ext := parser.Search(download.Code("https://prnt.sc/" + path))
-	//fmt.Println(imgURL, ext)
-	if imgURL[0] == 47 {
-		fmt.Println("nihuya")
-	} else if ext == "peg" {
-		download.File(imgURL, path+".jpeg")
-		fmt.Println(path + ".jpeg")
-	} else {
-		download.File(imgURL, path+"."+ext)
-		fmt.Println(path + ".jpeg")
+
+	for i := 0; i < 1000; i++ {
+		path := randomizer.String(6)
+		//fmt.Println(path)
+		//s := "https://prnt.sc/"
+		//download.File(s+path, path+".html")
+		/*if err := download.File("https://prnt.sc/"+path, path+".html"); err != nil {
+			panic(err)
+		}*/
+		imgURL, ext := parser.Search(download.Code("https://prnt.sc/" + path))
+		//fmt.Println(imgURL, ext)
+		if (imgURL[0] == 47) || (ext == "here") {
+			fmt.Println("nihuya")
+		} else if ext == "peg" {
+			download.File(imgURL, path+".jpeg")
+			fmt.Println(path + ".jpeg")
+		} else {
+			download.File(imgURL, path+"."+ext)
+			fmt.Println(path + "." + ext)
+		}
 	}
 }

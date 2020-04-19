67b54e20515b3a7f409628cea0b56623f3bb6977
Mike M <mike@makarov.pw>
2020-04-19 07:16:04 +0300

    before adding the cycles

--- parser/parser.go
+++ parser/parser.go
@@ -10,5 +10,9 @@ func Search(input string) (string, string) {
 	queryEnd, _ := regexp.Compile("<meta property=")
 	foundEnd := queryEnd.FindStringIndex(input)
 	runes := []rune(input)
-	return string(runes[foundBegin[1]+1 : foundEnd[0]-4]), string(runes[foundEnd[0]-7 : foundEnd[0]-4])
+	if (foundBegin != nil) && (foundEnd != nil) {
+		return string(runes[foundBegin[1]+1 : foundEnd[0]-4]), string(runes[foundEnd[0]-7 : foundEnd[0]-4])
+	} else {
+		return "nothing", "here"
+	}
 }
--- prntscr.go
+++ prntscr.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 
 	"makarov.pw/prntscr/download"
 	"makarov.pw/prntscr/parser"
@@ -11,30 +10,21 @@ import (
 
 func main() {
 	path := randomizer.String(6)
-	fmt.Println(path)
-	PrintMemUsage()
+	//fmt.Println(path)
 	//s := "https://prnt.sc/"
 	//download.File(s+path, path+".html")
 	/*if err := download.File("https://prnt.sc/"+path, path+".html"); err != nil {
 		panic(err)
 	}*/
 	imgURL, ext := parser.Search(download.Code("https://prnt.sc/" + path))
-	PrintMemUsage()
-	fmt.Println(imgURL, ext)
-	download.File(imgURL, path+"."+ext)
-	PrintMemUsage()
-}
-
-func PrintMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
+	//fmt.Println(imgURL, ext)
+	if imgURL[0] == 47 {
+		fmt.Println("nihuya")
+	} else if ext == "peg" {
+		download.File(imgURL, path+".jpeg")
+		fmt.Println(path + ".jpeg")
+	} else {
+		download.File(imgURL, path+"."+ext)
+		fmt.Println(path + ".jpeg")
+	}
 }
